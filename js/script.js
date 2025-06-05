6df5cab25e7dd0fc211a9e1530b9daeb125dc7b8
ywcho97 <111740922+ywcho97@users.noreply.github.com>
2025-06-05 16:35:14 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -138,3 +138,21 @@ window.addEventListener("load", () => {
     // 이미지 설정
     introImg.src = selectedImage;
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const fadeUps = document.querySelectorAll('.fade-up');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    fadeUps.forEach(el => observer.observe(el));
+});
